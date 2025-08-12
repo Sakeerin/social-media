eddfb60dcce661a0ff0c289780fd43cbbd18405b
Sakeerin <theblue0800@gmail.com>
2025-08-12 10:11:01 +0700

    Create Models

---
+++ app/Models/Comment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasUuids;
+
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'content',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
+
---
+++ app/Models/Like.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    use HasUuids;
+
+    protected $fillable = [
+        'post_id',
+        'user_id',
+    ];
+}
